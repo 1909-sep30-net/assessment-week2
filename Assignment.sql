feada1557e2063dd94d362009ef6eabddb279909
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-14 11:07:42 -0500

    Add Manipulation and Queries

--- Assignment.sql
+++ Assignment.sql
@@ -1,14 +1,14 @@
 --creation
 CREATE TABLE Products
 (
-	ProductId INT PRIMARY KEY IDENTITY,
+	ProductId INT PRIMARY KEY IDENTITY(1,1),
 	Name NVARCHAR(64) NOT NULL,
 	Price MONEY NOT NULL
 )
 
 CREATE TABLE Customers
 (
-	CustomerId INT PRIMARY KEY IDENTITY,
+	CustomerId INT PRIMARY KEY IDENTITY(1,1),
 	FirstName NVARCHAR(64) NOT NULL,
 	LastName NVARCHAR(64) NOT NULL,
 	CardNumber NVARCHAR(32) NOT NULL
@@ -16,11 +16,11 @@ CREATE TABLE Customers
 
 CREATE TABLE Orders
 (
-	OrderId INT PRIMARY KEY IDENTITY,
+	OrderId INT PRIMARY KEY IDENTITY(1,1),
 	ProductId INT FOREIGN KEY REFERENCES Products (ProductId),
 	CustomerId INT FOREIGN KEY REFERENCES Customers (CustomerId)
 )
-
+--DROP TABLE Products DROP TABLE Customers DROP TABLE Orders
 --Manipulation
 INSERT INTO Products VALUES
 	('Coke',1.99),
@@ -34,8 +34,37 @@ INSERT INTO Orders VALUES
 	(1,1), --greg coke
 	(2,3), --tracy water
 	(3,1)	--richard coke
+SELECT * FROM Products
+SELECT * FROM Customers
+SELECT * FROM Orders
+
 
 INSERT INTO Products VALUES ('iPhone', 200.00)
 
 INSERT INTO Customers VALUES ('Tina', 'Smith', '3456')
+
+INSERT INTO Orders VALUES (4,4) --tina, iphone
+
+SELECT c.FirstName AS [First Name], c.LastName AS [Last Name], o.OrderId AS [OrderId]
+FROM Customers AS c INNER JOIN Orders AS o ON c.CustomerId = o.CustomerId
+Where c.CustomerId = 4;
+
+SELECT SUM(p.Price) AS [Revenue of iPhone]
+FROM Orders AS o INNER JOIN Products as p ON o.ProductId = p.ProductId
+WHERE p.ProductId = 4
+
+UPDATE Products 
+SET Price = 250.00
+WHERE ProductId = 4
+
+
+
+
+
+
+
+
+
+
+
 
