2948a37e3a26c9cecc1a5bc4e67dc3b58f6f2972
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-14 10:50:51 -0500

    Create Table and Insertions

---
+++ Assignment.sql
@@ -0,0 +1,41 @@
+--creation
+CREATE TABLE Products
+(
+	ProductId INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(64) NOT NULL,
+	Price MONEY NOT NULL
+)
+
+CREATE TABLE Customers
+(
+	CustomerId INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(64) NOT NULL,
+	LastName NVARCHAR(64) NOT NULL,
+	CardNumber NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Orders
+(
+	OrderId INT PRIMARY KEY IDENTITY,
+	ProductId INT FOREIGN KEY REFERENCES Products (ProductId),
+	CustomerId INT FOREIGN KEY REFERENCES Customers (CustomerId)
+)
+
+--Manipulation
+INSERT INTO Products VALUES
+	('Coke',1.99),
+	('Pepsi',1.89),
+	('Water',1.49)
+INSERT INTO Customers VALUES
+	('Greg','Favrot','1234'),
+	('Tracy','Yoes','2345'),
+	('Richard','Favrot', '2345')
+INSERT INTO Orders VALUES
+	(1,1), --greg coke
+	(2,3), --tracy water
+	(3,1)	--richard coke
+
+INSERT INTO Products VALUES ('iPhone', 200.00)
+
+INSERT INTO Customers VALUES ('Tina', 'Smith', '3456')
+
